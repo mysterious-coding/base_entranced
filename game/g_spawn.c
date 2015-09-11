289c6506d6292bd0dab8ac26d0b438fd707239c2
deathsythe47 <deathsythe47@gmail.com>
2015-09-11 00:02:44 -0700

    control g_siegerespawn and g_siegeteamswitch in worldspawn

--- game/g_spawn.c
+++ game/g_spawn.c
@@ -1335,6 +1335,12 @@ void SP_worldspawn( void )
 	G_SpawnString("forceOnNPCs", "0", &text); //if not defined, set to 0
 	trap_Cvar_Set("g_forceOnNPCs", text);
 
+	G_SpawnString("siegeRespawn", "20", &text); //if not defined, set to 20
+	trap_Cvar_Set("g_siegeRespawn", text);
+
+	G_SpawnString("siegeTeamSwitch", "1", &text); //if not defined, set to 1
+	trap_Cvar_Set("g_siegeTeamSwitch", text);
+
 	G_SpawnString( "enableBreath", "0", &text );
 	trap_Cvar_Set( "g_enableBreath", text );
 
