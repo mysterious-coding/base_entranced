7ddb0ed9c8f11bf31e0509ec1e695a4b7388d82e
deathsythe47 <deathsythe47@gmail.com>
2015-10-04 14:28:08 -0700

    Revert "fix siege teamchat during countdown"
    This reverts commit 0ba70e361b85dbed40e795efa6ca89e9f493c4e6.

--- game/g_cmds.c
+++ game/g_cmds.c
@@ -1649,13 +1649,8 @@ static void G_SayTo( gentity_t *ent, gentity_t *other, int mode, int color, cons
 	if ( other->client->pers.connected != CON_CONNECTED ) {
 		return;
 	}
-	if (mode == SAY_TEAM) {
-		if (!OnSameTeam(ent, other))
-			return;
-
-		if ((g_gametype.integer == GT_SIEGE)
-			&& (ent->client->sess.siegeDesiredTeam != other->client->sess.siegeDesiredTeam))
-			return;
+	if ( mode == SAY_TEAM  && !OnSameTeam(ent, other) ) {
+		return;
 	}
 	if ((other->client->sess.ignoreFlags & (1<<(ent-g_entities)))
 		&& (ent!=other)	){
