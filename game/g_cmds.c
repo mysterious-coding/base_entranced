a7f18cb65fc19da6314da8457cf118f95d7717e2
Avygeil <avyg31l@gmail.com>
2016-03-03 18:12:30 +0100

    Maybe use the cvar :))

--- game/g_cmds.c
+++ game/g_cmds.c
@@ -1978,6 +1978,10 @@ static char* GetPrefixId( gentity_t *ent ) {
 	int i;
 	gentity_t *other;
 
+	if ( !g_duplicateNamesId.integer ) {
+		return "";
+	}
+
 	for ( i = 0; i < level.maxclients; i++ ) {
 		other = &g_entities[i];
 
