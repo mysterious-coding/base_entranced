da083d705daffab5cfbcbd6a79cbd8736671f831
deathsythe47 <deathsythe47@gmail.com>
2016-03-15 18:24:43 -0500

    Revert "more naming improvements, add rename"
    This reverts commit 8121d748ca7e2a2344df78fc36c85d294fb736b2.

--- game/g_client.c
+++ game/g_client.c
@@ -11,8 +11,6 @@
 #define POWER_DUEL_START_HEALTH 150
 #define POWER_DUEL_END_HEALTH 90
 
-#define RENAME_TIME 700
-
 // g_client.c -- client functions that don't happen every frame
 
 static vec3_t	playerMins = {-15, -15, DEFAULT_MINS_2};
@@ -1397,14 +1395,6 @@ static void ClientCleanName(const char *in, char *out, int outSize)
 
 	out[outpos] = '\0';
 
-	// don't allow empty names
-	if (*out == '\0' || colorlessLen == 0)
-	{
-		Q_strncpyz(out, "Padawan", outSize);
-		return;
-	}
-
-	// discard trailing spaces
 	int i = outpos - 1;
 	while (out[i] == ' ')
 	{
@@ -1965,6 +1955,7 @@ void ClientUserinfoChanged( int clientNum ) {
 		ClientCleanName( s, client->pers.netname, sizeof(client->pers.netname) );
 	}
 
+
 	if ( client->sess.sessionTeam == TEAM_SPECTATOR ) {
 		if ( client->sess.spectatorState == SPECTATOR_SCOREBOARD ) {
 			Q_strncpyz( client->pers.netname, "scoreboard", sizeof(client->pers.netname) );
@@ -1977,14 +1968,7 @@ void ClientUserinfoChanged( int clientNum ) {
 		{
 			if ( client->pers.netnameTime > level.time  )
 			{
-				if (client->pers.netnameTime > RENAME_TIME)
-				{
-					//trap_SendServerCommand(ent - g_entities, va("print \"You have been renamed recently; you cannot change your name yet.\n\""));
-				}
-				else
-				{
-					trap_SendServerCommand(clientNum, va("print \"%s\n\"", G_GetStringEdString("MP_SVGAME", "NONAMECHANGE")));
-				}
+				trap_SendServerCommand( clientNum, va("print \"%s\n\"", G_GetStringEdString("MP_SVGAME", "NONAMECHANGE")) );
 				
 				Info_SetValueForKey( userinfo, "name", oldname );
 				trap_SetUserinfo( clientNum, userinfo );	
@@ -2009,7 +1993,7 @@ void ClientUserinfoChanged( int clientNum ) {
 				trap_SendServerCommand( -1, va("print \"%s" S_COLOR_WHITE " %s %s\n\"", oldname, G_GetStringEdString("MP_SVGAME", "PLRENAME"), client->pers.netname) );
 				G_LogPrintf("Client num %i from %s renamed from '%s' to '%s'\n", clientNum,client->sess.ipString,
 					oldname, client->pers.netname);
-				client->pers.netnameTime = level.time + RENAME_TIME; //change time limit from 5s to 1s
+				client->pers.netnameTime = level.time + 700; //change time limit from 5s to 1s
 
 				//weird rename bug fix, after we change name, we should update userinfo
 				Info_SetValueForKey( userinfo, "name", client->pers.netname );
