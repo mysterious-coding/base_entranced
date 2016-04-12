cf07aa7bbf215a1e11a3e08d7ca7c05cce1b732e
deathsythe47 <deathsythe47@gmail.com>
2016-04-11 19:44:05 -0500

    fix derp

--- game/g_saga.c
+++ game/g_saga.c
@@ -1828,7 +1828,7 @@ void SiegeObjectiveCompleted(int team, int objective, int final, int client)
 {
 	int goals_completed, goals_required;
 
-	if (client && client >= 0 && client < MAX_CLIENTS && &level.clients[client] && level.clients[client].pers.connected == CON_CONNECTED)
+	if (client >= 0 && client < MAX_CLIENTS && &level.clients[client] && level.clients[client].pers.connected == CON_CONNECTED)
 	{
 		level.clients[client].pers.teamState.captures++;
 		level.clients[client].rewardTime = level.time + REWARD_SPRITE_TIME;
@@ -1837,7 +1837,7 @@ void SiegeObjectiveCompleted(int team, int objective, int final, int client)
 
 	SiegeUpdateObjTime(objective, qfalse); //we just completed an obj, so let's write down the time it took for this obj
 
-	if (client && objective && &g_entities[client] && g_entities[client].client)
+	if (client >= 0 && objective && &g_entities[client] && g_entities[client].client)
 	{
 		G_LogPrintf("Objective %i completed by client %i (%s)\n", objective, client, g_entities[client].client->pers.netname);
 	}
