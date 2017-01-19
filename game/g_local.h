5ea2be81da96d4cc6153e0dc6b324cd70a1173d5
Avygeil <avyg31l@gmail.com>
2017-01-19 10:27:59 +0100

    switched to base64url encoding set to avoid double slashes in cvars

--- game/g_local.h
+++ game/g_local.h
@@ -1176,7 +1176,7 @@ typedef struct {
 
 #ifdef NEWMOD_SUPPORT
 	qboolean nmAuthEnabled;
-	char pubKeyStr[RSA_MAX_PUB_B64_CHARS];
+	char pubKeyStr[RSA_MAX_PUB_B64U_CHARS];
 #endif
 
 } level_locals_t;
