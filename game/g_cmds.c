eb45c2d7a990ff863176e5496accb76430adccf3
deathsythe47 <deathsythe47@gmail.com>
2017-12-17 12:14:50 -0600

    207

--- game/g_cmds.c
+++ game/g_cmds.c
@@ -6189,7 +6189,7 @@ void ServerCfgColor(char *string, int integer, gentity_t *ent)
 	trap_SendServerCommand(ent - g_entities, va("print \"%s %i\n\"", string, integer));
 }
 
-#define BUILDNUMBER	206
+#define BUILDNUMBER	207
 
 void Cmd_Help_f(gentity_t *ent)
 {
