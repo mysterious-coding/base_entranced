dpadgett/base_enhanced-1

test saber def changes for siege (https://forums.jasiege.com/phpBB3/viewtopic.php?f=18&t=133#p496) these changes are not meant for CTF

game/g_local.h
@@ -1719,7 +1719,7 @@ qboolean HasSetSaberOnly(void);
 void WP_ForcePowerStop( gentity_t *self, forcePowers_t forcePower );
 void WP_ForcePowerStart( gentity_t *self, forcePowers_t forcePower, int overrideAmt );
 void WP_SaberPositionUpdate( gentity_t *self, usercmd_t *ucmd );
-int WP_SaberCanBlock(gentity_t *self, vec3_t point, int dflags, int mod, qboolean projectile, int attackStr);
+int WP_SaberCanBlock(gentity_t *self, gentity_t *other, vec3_t point, int dflags, int mod, qboolean projectile, int attackStr);
 void WP_SaberInitBladeData( gentity_t *ent );
 void WP_InitForcePowers( gentity_t *ent );
 void WP_SpawnInitForcePowers( gentity_t *ent );
@@ -1878,6 +1878,9 @@ extern	vmCvar_t	g_g2TraceLod;
 extern	vmCvar_t	g_optvehtrace;
 
 extern	vmCvar_t	g_locationBasedDamage;
+
+extern	vmCvar_t	g_disruptorGhoul2Blocks;
+extern	vmCvar_t	g_disruptorOnlyDef3Blocks;
 
 extern	vmCvar_t	g_allowHighPingDuelist;
 

mysterious-coding/base_entranced

game/g_local.h
@@ -2101,7 +2101,7 @@ qboolean HasSetSaberOnly(void);
 void WP_ForcePowerStop( gentity_t *self, forcePowers_t forcePower );
 void WP_ForcePowerStart( gentity_t *self, forcePowers_t forcePower, int overrideAmt );
 void WP_SaberPositionUpdate( gentity_t *self, usercmd_t *ucmd );
-int WP_SaberCanBlock(gentity_t *self, vec3_t point, int dflags, int mod, qboolean projectile, int attackStr);
+int WP_SaberCanBlock(gentity_t *self, gentity_t *other, vec3_t point, int dflags, int mod, qboolean projectile, int attackStr);
 void WP_SaberInitBladeData( gentity_t *ent );
 void WP_InitForcePowers( gentity_t *ent );
 void WP_SpawnInitForcePowers( gentity_t *ent );
@@ -2260,6 +2260,12 @@ extern	vmCvar_t	g_g2TraceLod;
 extern	vmCvar_t	g_optvehtrace;
 
 extern	vmCvar_t	g_locationBasedDamage;
+
+extern	vmCvar_t	g_fixSaberDefense;
+extern	vmCvar_t	g_saberDefense1Angle;
+extern	vmCvar_t	g_saberDefense2Angle;
+extern	vmCvar_t	g_saberDefense3Angle;
+extern	vmCvar_t	g_saberDefensePrintAngle;
 
 extern	vmCvar_t	g_allowHighPingDuelist;
 