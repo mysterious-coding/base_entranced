d877c3996dfa4527f68b5e2bc44e27f725a8dd9c
deathsythe47 <deathsythe47@gmail.com>
2019-05-03 16:19:38 -0700

    fix merge 2

--- game/g_main.c
+++ game/g_main.c
@@ -782,7 +782,7 @@ static cvarTable_t		gameCvarTable[] = {
 	{ &g_antiWallhack,	"g_antiWallhack"	, "0"	, CVAR_ARCHIVE, 0, qtrue },
 	{ &g_wallhackMaxTraces,	"g_wallhackMaxTraces"	, "1000"	, CVAR_ARCHIVE, 0, qtrue },
 
-    { &g_restart_countdown, "g_restart_countdown", "5", CVAR_ARCHIVE, 0, qtrue }, 
+    { &g_restart_countdown, "g_restart_countdown", "0", CVAR_ARCHIVE, 0, qtrue }, 
 
 	{ &g_allow_vote_customTeams,	"g_allow_vote_customTeams"	, "0"	, CVAR_ARCHIVE, 0, qtrue },
 	{ &g_allow_vote_gametype,	"g_allow_vote_gametype"	, "1023"	, CVAR_ARCHIVE, 0, qtrue },
