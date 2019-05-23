a64e2e4e95656d354e3ec2ecb64ac6a69d393977
deathsythe47 <deathsythe47@gmail.com>
2019-05-22 21:08:08 -0700

    317

--- game/g_local.h
+++ game/g_local.h
@@ -33,7 +33,7 @@ extern vec3_t gPainPoint;
 // the "gameversion" client command will print this plus compile date
 #define	GAMEVERSION	"base_entranced"
 //#define	GAMEVERSION	"basejka" //test
-#define MODBUILDNUMBER			"316"
+#define MODBUILDNUMBER			"317"
 #define GAMEVERSION_VALUE		GAMEVERSION" "MODBUILDNUMBER
 
 #define DEFAULT_NAME			S_COLOR_WHITE"Padawan"
