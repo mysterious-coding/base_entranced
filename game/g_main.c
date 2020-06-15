e452d05bc856173d2f3302a94086fa04e8b7b3ac
deathsythe47 <deathsythe47@gmail.com>
2020-06-14 19:00:52 -0700

    hopefully fix map change crash

--- game/g_main.c
+++ game/g_main.c
@@ -2422,6 +2422,8 @@ void G_InitGame( int levelTime, int randomSeed, int restart ) {
 
 	ClearRegisteredItems();
 
+	G_DBLoadDatabase();
+
 	//make sure saber data is loaded before this! (so we can precache the appropriate hilts)
 	InitSiegeMode();
 
@@ -2520,7 +2522,6 @@ void G_InitGame( int levelTime, int randomSeed, int restart ) {
 		PatchEngine();
 	}
 
-	G_DBLoadDatabase();
     //level.db.levelId = G_LogDbLogLevelStart(restart);
 
 	InitializeSiegeHelpMessages();
